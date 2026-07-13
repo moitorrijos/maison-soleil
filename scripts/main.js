338917ff7f111f5d4d82161d072f6c75534bad9b
Juan M. Torrijos <juanmtorrijos@gmail.com>
2026-07-13 16:13:47 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const mainContainer = document.querySelector('.main-container');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelectorAll('.close-button');
+
+function toggleSidebar() {
+    mainContainer.classList.toggle('sidebar-visible');
+    overlay.classList.toggle('visible');
+}
+
+closeButton.forEach(button => {
+    button.addEventListener('click', () => {
+        toggleSidebar();
+    });
+});
+
+overlay.addEventListener('click', () => {
+    toggleSidebar();
+});
